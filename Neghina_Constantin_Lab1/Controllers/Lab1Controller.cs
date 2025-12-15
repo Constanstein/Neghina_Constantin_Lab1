2c873e54ec6311e4d32ebc1bbe8f624f601e3201
Constanstein <neghinaconstantin1@gmail.com>
2025-12-15 18:55:47 +0300

    Adaugat metode Lab1

---
+++ Neghina_Constantin_Lab1/Controllers/Lab1Controller.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Neghina_Constantin_Lab1.Controllers
+{
+    public class Lab1Controller : Controller
+    {
+        public string Index()
+        {
+            return "Bun venit tuturor!";
+        }
+
+        public string Salut()
+        {
+            return "Va salut pe toti!";
+        }
+
+        public string Info(string nume, int varsta)
+        {
+            return "Numele este " + nume + " si are " + varsta + " ani.";
+        }
+
+    }
+}

aa7c066a94d17d461a51904411cc85d5807cd626
Constanstein <neghinaconstantin1@gmail.com>
2025-12-15 19:43:50 +0300

    Am adaugat Exercitiul 2

---
+++ Neghina_Constantin_Lab1/Controllers/Exercitiu2Controller.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Neghina_Constantin_Lab1.Controllers
+{
+    public class Exercitiu2Controller : Controller
+    {
+        public IActionResult Date()
+        {
+            ViewBag.Message = "Mesaj din Controller";
+            ViewData["Time"] = DateTime.Now; 
+            return View();
+        }
+    }
+}
